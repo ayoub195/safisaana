220983675dbc8e1ecf60c7d2c7df290a4816d2e9
Ayoub <mariambintiyako@gmail.com>
2025-04-09 21:11:42 +0300

    chore: configure next.js and eslint for production build

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  extends: ['next/core-web-vitals'],
+  rules: {
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+    'react/no-unescaped-entities': 'off',
+    'react-hooks/exhaustive-deps': 'off',
+    '@next/next/no-img-element': 'off'
+  },
+  ignorePatterns: [
+    'node_modules/*',
+    '.next/*',
+    'out/*',
+    'public/*',
+    '**/*.d.ts'
+  ]
+} 
